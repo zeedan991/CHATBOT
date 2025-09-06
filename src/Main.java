930d7312686a2e871e41449dcff383cca7c49842
zedaan farooqe khatik <zedaankhatik@gmail.com>
2025-09-06 15:33:35 +0530

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main  {
+    public static void main(String[] args) throws InterruptedException {
+        ChatUser u1 = new ChatUser("Bob");
+        ChatUser u2 = new ChatUser("Alice");
+        u1.setPriority(Thread.MAX_PRIORITY);
+        u2.setPriority(Thread.MIN_PRIORITY);
+        u1.start();
+        u2.start();
+        System.out.println("Is alice alive : " +u2.isAlive());
+        Thread.sleep(500);
+        u1.Pausechat();
+        System.out.println("bob paused ");
+        u2.join();
+        u1.Resumechat();
+        System.out.println("bob resumed ");
+        Thread.sleep(500);
+        u1.Stopchat();
+        u1.join();
+        Thread.sleep(50);
+        System.out.println("alice after join "+u2.isAlive());
+        System.out.println("Chat ended ");
     }
+}
+class ChatUser extends Thread{
+    private volatile boolean paused = false;
+    private volatile boolean running = true;
+    ChatUser(String name){
+        super(name);
+    }
+    @Override
+    public void run(){
+        int i = 1;
+        while(running&&i<=5){
+            if(!paused){System.out.println(getName()+ " says : Message " + i);
+                i++;
+                try{
+                    Thread.sleep(500);}
+                catch (Exception e){System.out.println(e.getMessage());}
+            }
+        }System.out.println(getName()+ " Finished Chatting");
+    }
+    public void Pausechat(){paused = true;}
+    public void Stopchat(){running = false;}
+    public void Resumechat(){paused = false;}
 }
